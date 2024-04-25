e30971cdf18249c8b55127e16d0bd0cf5445816a
Anurag Arwalkar <anurag.arwalkar@globant.com>
2024-04-25 23:40:19 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Box minH={'100vh'} bg={'#0f0a19'} color='gray.500' px={4} py={8}> </Box>
+    </main>
+  );
+}
